a87aad7f87fe8bf7555926dc854644cfd524a402
felix-hauger <felix.hauger@laplateforme.io>
2023-05-03 11:26:19 +0200

    index.php - use FakerPHP to generate users on users/fill route

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Faker\Factory;
+
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $router = new AltoRouter();
@@ -20,6 +22,30 @@ $router->map('GET', '/users', function() {
 $router->map('GET', '/users/[i:id]', function($id) {
     echo 'Bonjour utilisateur ' . $id;
 }, 'user_page');
+
+$router->map('GET', '/users/fill', function() {
+    $pdo = new PDO('mysql:host=localhost;dbname=superweek;charset=utf8mb4', 'root', '');
+
+    $sql = 'INSERT INTO user (email, password, first_name, last_name) VALUES (:email, :password, :first_name, :last_name)';
+
+    $insert = $pdo->prepare($sql);
+
+    for ($i = 0; $i < 30; $i++) {
+        $faker = Faker\Factory::create('fr_FR');
+
+        $first_name = $faker->firstName();
+        $last_name = $faker->lastName();
+        $email = strtolower($first_name . '.' . $last_name) . '@' . $faker->freeEmail();
+        $password = password_hash($first_name, PASSWORD_DEFAULT);
+
+        $insert->execute([
+            ':email' => $email,
+            ':password' => $password,
+            ':first_name' => $first_name,
+            ':last_name' => $last_name,
+        ]);
+    }
+});
 
 // match current request url
 $match = $router->match();
