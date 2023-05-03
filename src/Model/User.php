1cffe491e65cee58065080eb3f6663a3cdb74e17
felix-hauger <felix.hauger@laplateforme.io>
2023-05-03 13:58:06 +0200

    Model/User - put database connection inside a try..catch + add comments

--- src/Model/User.php
+++ src/Model/User.php
@@ -3,20 +3,28 @@
 namespace App\Model;
 
 use PDO;
+use PDOException;
 
 class User
 {
     private $_db;
 
+    /**
+     * Set Database connection & store it in $_db property
+     */
     public function __construct()
     {
-        // get database infos from ini file in config folder
-        $db = parse_ini_file(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR .  'db.ini');
-
-        // define PDO dsn & auth infos with retrieved data
-        $this->_db = new PDO($db['type'] . ':dbname=' . $db['name'] . ';host=' . $db['host'] . ';charset=' . $db['charset'], $db['user'], $db['password']);
-
-        // prevent emulation of prepared requests
-       $this->_db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        try {
+            // Get database infos from ini file in config folder
+            $db = parse_ini_file(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR .  'db.ini');
+    
+            // Define PDO dsn & auth infos with retrieved data
+            $this->_db = new PDO($db['type'] . ':dbname=' . $db['name'] . ';host=' . $db['host'] . ';charset=' . $db['charset'], $db['user'], $db['password']);
+    
+            // Prevent emulation of prepared requests
+           $this->_db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
 }
