eac57fc84c5167b6b1f0bbd68930dc1611b79c74
felix-hauger <felix.hauger@laplateforme.io>
2023-05-05 13:29:02 +0200

    Controller/User::fill() - Remove accents with iconv instead of strtr & instanciate Faker\Factory outside of the loop to save memory

--- src/Controller/User.php
+++ src/Controller/User.php
@@ -22,20 +22,14 @@ class User
 
         $user = new EntityUser();        
 
+        $faker = \Faker\Factory::create('fr_FR');
 
         for ($i = 0; $i < 30; $i++) {
-            $faker = \Faker\Factory::create('fr_FR');
-    
-            $unwanted_chars = [
-                'À'=>'A', 'Â'=>'A', 'Ä'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
-                'Ê'=>'E', 'Ë'=>'E', 'Î'=>'I', 'Ï'=>'I', 'Ô'=>'O', 'Ö'=>'O', 'Ù'=>'U',
-                'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'à'=>'a', 'â'=>'a', 'ä'=>'a', 'æ'=>'a', 'ç'=>'c',
-                'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'î'=>'i', 'ï'=>'i', 'ñ'=>'n', 'ô'=>'o'
-            ];
     
             $first_name = $faker->firstName();
             $last_name = $faker->lastName();
-            $email = strtolower(strtr($first_name . '.' . $last_name, $unwanted_chars)) . '@' . $faker->freeEmailDomain();
+            // Convert UTF-8 characters to ASCII to create email without accents
+            $email = strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $first_name . '.' . $last_name)) . '@' . $faker->freeEmailDomain();
             $password = password_hash($first_name, PASSWORD_DEFAULT);
 
             $user
