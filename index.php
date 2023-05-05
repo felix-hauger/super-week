e891107ec0c1bd30f70d2edacda9814be0430ae3
felix-hauger <felix.hauger@laplateforme.io>
2023-05-05 19:58:59 +0200

    Remove old index.php file at project root

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-
-$router = new AltoRouter();
-
-$router->setBasePath(DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR);
-
-echo 'coucou';
