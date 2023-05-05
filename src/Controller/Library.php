0fb1c2f88248d8ffb893523644fe15052cb66aba
felix-hauger <felix.hauger@laplateforme.io>
2023-05-05 20:45:41 +0200

    Add Controller/Library::writeBook() method

--- src/Controller/Library.php
+++ src/Controller/Library.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Model\Book as ModelBook;
+use FFI\Exception;
+
 /**
  * Class managing books & library
  * 
@@ -17,5 +21,32 @@ class Library
     public function getWriteForm(): void
     {
         require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'write-book.php';
+    }
+
+    public function writeBook()
+    {
+        $input = [
+            'title' => filter_input(INPUT_POST, 'title', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'summary' => filter_input(INPUT_POST, 'summary', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+            'content' => filter_input(INPUT_POST, 'content', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+        ];
+
+        foreach ($input as $key => $value) {
+            if (!$value) {
+                throw new Exception(str_replace('_', ' ', ucwords($key)) . ' input invalid.');
+            }
+        }
+
+        $user_model = new ModelBook();
+
+        $user = new Book();
+
+        $user
+            ->setTitle($input['title'])
+            ->setSummary($input['summary'])
+            ->setContent($input['content'])
+            ->setUserId($_SESSION['user']->getId());
+
+        return $user_model->create($user);
     }
 }
--- src/View/write-book.php
+++ src/View/write-book.php
@@ -1,7 +1,7 @@
 <form method="post">
     <input type="text" name="title" id="title" placeholder="Title">
-    <textarea name="" id="" cols="20" rows="8" placeholder="Summary"></textarea>
-    <textarea name="" id="" cols="40" rows="20" placeholder="Write your book here..."></textarea>
+    <textarea name="summary" id="summary" cols="20" rows="8" placeholder="Summary"></textarea>
+    <textarea name="content" id="content" cols="40" rows="20" placeholder="Write your book here..."></textarea>
     <input type="submit" name="submit" value="Submit">
     <?php if (isset($register_error)): ?>
         <p><?= $register_error ?></p>
