32bd96b74147e9e4c1abf3ca50caa73cc06d5841
felix-hauger <felix.hauger@laplateforme.io>
2023-05-05 20:13:46 +0200

    Add method to require book writing form in Controller/Library

--- src/Controller/Library.php
+++ src/Controller/Library.php
@@ -2,7 +2,20 @@
 
 namespace App\Controller;
 
+/**
+ * Class managing books & library
+ * 
+ * @package Library
+ * 
+ * @method void getWriteForm()
+ */
 class Library
 {
-    
+    /**
+     * Require book writing form, to be used in the adequate route
+     */
+    public function getWriteForm(): void
+    {
+        require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'write-book.php';
+    }
 }
