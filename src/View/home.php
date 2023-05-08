57ee402769bb1699445f04e8fe14fe58a1411159
felix-hauger <felix.hauger@laplateforme.io>
2023-05-08 12:15:22 +0200

    Add html buttons & container for homepage

--- src/View/home.php
+++ src/View/home.php
@@ -8,5 +8,19 @@
 </head>
 <body>
     <h1><?= isset($_SESSION['user']) ? 'Welcome ' . $_SESSION['user']->getFirstName() : 'Welcome to homepage' ?></h1>
+
+    <div id="buttons">
+        <button id="get-users">Display all users</button>
+
+        <button id="get-books">Display all books</button>
+
+        <input type="number" name="user-id" id="user-id" placeholder="User Id">
+        <button id="get-users">Display user infos</button>
+
+        <input type="number" name="book-id" id="book-id" placeholder="Book Id">
+        <button id="get-users">Display book infos</button>
+    </div>
+
+    <div id="content-container"></div>
 </body>
 </html>
