8ee640e2f5bcb59ca9688fa2efaf0fd8321390f3
felix-hauger <felix.hauger@laplateforme.io>
2023-05-08 16:05:36 +0200

    Display all users on home page at #get-all-users button click

--- src/View/home.php
+++ src/View/home.php
@@ -10,17 +10,38 @@
     <h1><?= isset($_SESSION['user']) ? 'Welcome ' . $_SESSION['user']->getFirstName() : 'Welcome to homepage' ?></h1>
 
     <div id="buttons">
-        <button id="get-users">Display all users</button>
+        <button id="get-all-users">Display all users</button>
 
-        <button id="get-books">Display all books</button>
+        <button id="get-all-books">Display all books</button>
 
         <input type="number" name="user-id" id="user-id" placeholder="User Id">
-        <button id="get-users">Display user infos</button>
+        <button id="get-user">Display user infos</button>
 
         <input type="number" name="book-id" id="book-id" placeholder="Book Id">
-        <button id="get-users">Display book infos</button>
+        <button id="get-user">Display book infos</button>
     </div>
 
     <div id="content-container"></div>
+
+    <script>
+        const
+            contentContainer = document.querySelector('#content-container'),
+            getAllUsersButton = document.querySelector('#get-all-users'),
+            getAllBooksButton = document.querySelector('#get-all-books'),
+            getUserButton = document.querySelector('#get-user'),
+            getBookButton = document.querySelector('#get-book');
+    
+        const getAllUsers = async function() {
+
+            const request = await fetch('/super-week/users');
+
+            const content = await request.text();
+
+            contentContainer.innerHTML = content;            
+        };
+
+        getAllUsersButton.addEventListener("click", getAllUsers);
+
+    </script>
 </body>
 </html>
